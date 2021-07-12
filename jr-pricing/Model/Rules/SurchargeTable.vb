f3b9655b01039d5c12bedc0b46a04548846b9860
HOMMA Takashi <homma@daniel-soft.com>
2021-07-13 07:09:31 +0900

    Rules追加

---
+++ jr-pricing/Model/Rules/SurchargeTable.vb
@@ -0,0 +1,20 @@
+﻿Imports Jr.Pricing.Model.Specification
+
+Namespace Model.Rules
+    ''' <summary>
+    ''' ひかり 指定席特急券テーブル
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Class SurchargeTable
+
+        Private ReadOnly map As New Dictionary(Of Destination, Integer) From {
+            {Destination.新大阪, 5490},
+            {Destination.姫路, 5920}
+        }
+
+        Public Function GetSurcharge(destination As Destination) As Integer
+            Return map(destination)
+        End Function
+
+    End Class
+End Namespace
