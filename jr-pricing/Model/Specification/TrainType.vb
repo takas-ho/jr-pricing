69f81398fabb0f4443bb8a16e8df5e08c820ffd5
HOMMA Takashi <homma@daniel-soft.com>
2021-07-13 06:36:24 +0900

    Specification追加

---
+++ jr-pricing/Model/Specification/TrainType.vb
@@ -0,0 +1,10 @@
+﻿Namespace Model.Specification
+    ''' <summary>
+    ''' 列車種類
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Enum TrainType
+        のぞみ
+        ひかり
+    End Enum
+End Namespace
