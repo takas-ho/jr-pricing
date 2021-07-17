318614f5c0500e2b739edce92f639500aa176c8a
HOMMA Takashi <homma@daniel-soft.com>
2021-07-17 09:54:43 +0900

    add TicketType

---
+++ jr-pricing/Model/Specification/TicketType.vb
@@ -0,0 +1,10 @@
+﻿Namespace Model.Specification
+    ''' <summary>
+    ''' 切符種類
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Enum TicketType
+        乗車券
+        特急券
+    End Enum
+End Namespace
