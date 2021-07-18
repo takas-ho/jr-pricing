aeed014d8cb56515ed554e8a667dfa8b9c50a12e
HOMMA Takashi <homma@daniel-soft.com>
2021-07-18 17:03:08 +0900

    10%割引+15%割引で「切り捨て」によって結果が変わる10020に変更する

--- jr-pricing/Model/Rules/FareTable.vb
+++ jr-pricing/Model/Rules/FareTable.vb
@@ -9,7 +9,7 @@ Namespace Model.Rules
 
         Private ReadOnly map As New Dictionary(Of Destination, Integer) From {
             {Destination.新大阪, 8910},
-            {Destination.姫路, 10010}
+            {Destination.姫路, 10020}
         }
 
         Public Function GetFare(destination As Destination) As Integer
